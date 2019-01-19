a2ab5ea328a4a941ebcd123d7935b8d7d433361b
petek157 <peterkoruga@Peters-iMac.attlocal.net>
2019-01-19 16:45:57 -0600

    wifi in setup

--- src/GarageDoorsAP.ino
+++ src/GarageDoorsAP.ino
@@ -15,11 +15,13 @@ bool leftOpen = false;
 const int rightTrigger = A1;
 bool rightOpen = false;
 
-WiFi.setCredentials("GLF", "abcdef1234");
-WiFi.setCredentials("IHV", "abcdef1234");
-WiFi.setCredentials("PKShop", "abcdef1234");
+
 
 void setup() {
+
+  WiFi.setCredentials("GLF", "abcdef1234");
+  WiFi.setCredentials("IHV", "abcdef1234");
+  WiFi.setCredentials("PKShop", "abcdef1234");
 
   //Wash
   Particle.variable("leftopen", leftOpen);
